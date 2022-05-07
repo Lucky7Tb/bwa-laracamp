d2e9a7fee59fe32df444cb710217fcb5682400f1
Lucky7Tb <Luckytri101@gmail.com>
2022-05-07 23:33:39 +0700

    slicing login view

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\LandingController::class, 'index']);
 
+Route::controller(\App\Http\Controllers\AuthController::class)
+    ->group(function() {
+        Route::get('login', 'showLogin')->name('view.login');
+    });
+
